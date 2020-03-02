e60c942d10e7b459d6b5076570d513b5f0594d73
sunshuhua <shuhua141@163.com>
2020-03-02 11:57:38 +0800

    rem适配-rootValue的兼容问题解决

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,21 @@
-// css后处理器的配置文件
-module.exports = {
-  plugins: {
-    'postcss-pxtorem': {
-      // rootValue是根值，是px向rem转换的砝码
-      // rem = px/rootValue  除法计算
-      // 200px/37.5 = 5.3333rem
-      // vant设计稿的大小是375px宽度的，rootValue就是设计稿的1/10，就是37.5
-      rootValue: 37.5,
-      propList: ['*']
+const {
+  sep
+} = require('path')
+
+module.exports = ({
+  file
+}) => {
+  const rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }
