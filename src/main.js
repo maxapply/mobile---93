cc8e893edb41ff17ab7f110c860794cc35b432a7
sunshuhua <shuhua141@163.com>
2020-03-02 10:42:52 +0800

    vant安装导入和简单使用

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// vant导入
+import Vant from 'vant'
+// vant的样式导入
+import 'vant/lib/index.css'
+// vant的注册
+// 本质：全局方式注册了n多的组件和全局成员
+//       Vue.component(xx,function(){})
+//       Vue.component(xx,function(){})
+//       Vue.prototype.xxx = yyy
+//       ……
+Vue.use(Vant)
 
+Vue.config.productionTip = false
 new Vue({
   router,
   store,
