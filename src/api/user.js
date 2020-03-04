3815e6cb4f51fa435c72e14701d8f9552f7f42a9
sunshuhua <shuhua141@163.com>
2020-03-04 11:42:50 +0800

    登录-封装api函数-参数升级

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,11 @@
 import request from '@/utils/request.js'
 
 // 导出一个函数，可以对用户账号进行校验
-export function apiUserLogin (data) {
+// data: {mobile:xx, code:xx} 对象参数
+// 现在对data做升级，使得在没有任何注释信息的条件下，也直接知道当前api函数需要什么参数，提高开发效率
+// 把参数做成是 “对象解构赋值” 的样子，这样就知道需要哪个参数了
+// export function apiUserLogin (data) {
+export function apiUserLogin ({ mobile, code }) {
   // 调用axios
   // axios.get()
   // axios.post()
@@ -17,6 +21,10 @@ export function apiUserLogin (data) {
   return request({
     url: '/app/v1_0/authorizations',
     method: 'POST',
-    data: data
+    data: {
+      // 简易成员赋值
+      mobile,
+      code
+    }
   })
 }
