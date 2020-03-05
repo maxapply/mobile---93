a0372996c186b21f7007833fe028fa3c990b2215
sunshuhua <shuhua141@163.com>
2020-03-05 11:54:48 +0800

    频道和文章-频道标签页组件应用

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class='container'>Home</div>
+  <div class='container'>
+    <!--
+      van-tabs:标签页组件
+        v-model设置默认激活项目 ，激活根据是项目的下标，从0开始
+      van-tab: 标签页项目组件
+        title：标签名称
+        内容区域：标签对应内容
+    -->
+    <van-tabs v-model="activeChannelIndex">
+      <van-tab title="推荐">推荐内容展示</van-tab>
+      <van-tab title="数据库">数据库内容展示</van-tab>
+      <van-tab title="后端">后端内容展示</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'home-index'
+  name: 'home-index',
+  data () {
+    return {
+      // 设置频道默认激活项目
+      activeChannelIndex: 1
+    }
+  }
 }
 </script>
 
