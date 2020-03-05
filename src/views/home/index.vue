e04360e7554683dfbf6f9bc1269e7a0b97d4e0b9
sunshuhua <shuhua141@163.com>
2020-03-05 14:46:01 +0800

    频道和文章-文章上拉-瀑布流组件说明

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,8 +11,30 @@
       <van-tab title="推荐">
         <!-- div的作用是给瀑布流区域设置“垂直滚动条”，使得可以进行上拉操作 -->
         <div class="scroll-wrapper">
-          <!-- 瀑布流加载效果实现 -->
+          <!-- 瀑布流加载效果实现
+            瀑布流执行：
+            1. 页面加载完毕，其会自动执行
+              (内部会判断当前瀑布区域没有数据，当然要执行获取数据)
+            2. 鼠标上拉动作，当滚动条到达"算法底部"，会执行
+               算法底部：瀑布流认为要加载更多数据的位置
+            v-model="loading"   表明是否是加载状态，默认为false
+                                // 加载的时候，自动设置为true
+                                // 加载完毕，请修改为false
+                                // 是加载动画的体现
+            :finished  表明瀑布停止了，没有更多数据不给加载了
+                      鼠标上拉已经没有反应了
+                      如果没有数据了，请设置finished为true
+                      就表示都加载好了，显示没有更多了
+
+            finished-text="没有更多了"  瀑布流停止，底部文字提示
+            @load="onLoad"  是事件，是做加载数据的时候执行的
+          -->
           <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+            <!-- van-cell单元格组件
+              特点：独占一行
+              用法非常灵活、复杂
+              title：单元格标题内容
+            -->
             <van-cell v-for="item in list" :key="item" :title="item"/>
           </van-list>
         </div>
@@ -30,14 +52,15 @@ export default {
   data () {
     return {
       // 瀑布流相关成员
-      list: [],
-      loading: false,
-      finished: false,
+      list: [], // 数据源
+      loading: false, // 瀑布流是否加载处于动画效果
+      finished: false, // 瀑布流是否停止
       // 设置频道默认激活项目
       activeChannelIndex: 0
     }
   },
   methods: {
+    // 瀑布流上拉执行的动作
     onLoad () {
       // 异步更新数据
       // setTimeout 仅做示例，真实场景中一般为 ajax 请求
@@ -51,6 +74,7 @@ export default {
 
         // 数据全部加载完成
         if (this.list.length >= 40) {
+          // 已经没有更多了，瀑布停止了
           this.finished = true
         }
       }, 1000)
