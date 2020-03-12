fff46bf8f6c36d1eb17d135bfa018c3436647ba2
sunshuhua <shuhua141@163.com>
2020-03-12 10:37:44 +0800

    搜索结果-获得数据-具体获取操作

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -5,8 +5,29 @@
 </template>
 
 <script>
+// 获得文章api
+import { apiSearchList } from '@/api/search.js'
 export default {
-  name: 'search-result'
+  name: 'search-result',
+  data () {
+    return {
+      searchList: [] // 根据关键字获得的文章列表信息
+    }
+  },
+  created () {
+    // 文章列表
+    this.getSearchList()
+  },
+  methods: {
+    // 获取文章
+    async getSearchList () {
+      // 调用api
+      const args = { q: this.$route.params.keyword }
+      const result = await apiSearchList(args)
+      // data接收
+      this.searchList = result.results
+    }
+  }
 }
 </script>
 
