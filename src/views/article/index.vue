d9ec91dadde68afcb18f96ba854c107273a6b081
sunshuhua <shuhua141@163.com>
2020-03-12 16:10:05 +0800

    文章详情-内容渲染-展示数据

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -3,34 +3,40 @@
     <van-nav-bar fixed left-arrow @click-left="$router.back()" title="文章详情"></van-nav-bar>
     <!-- 标题、作者头像、作者名称、文章发表时间、关注、文章详情、是否喜欢、点赞 -->
     <div class="detail">
-      <h3 class="title">美女与野兽</h3>
+      <h3 class="title">{{article.title}}</h3>
       <div class="author">
-        <van-image
-          round
-          width="1rem"
-          height="1rem"
-          fit="fill"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
-        />
+        <van-image round width="1rem" height="1rem" fit="fill" :src="article.aut_photo"/>
         <div class="text">
-          <p class="name">一阵清风</p>
-          <p class="time">两周内</p>
+          <p class="name">{{article.aut_name}}</p>
+          <p class="time">{{article.pubdate | formatTime}}</p>
         </div>
-        <van-button round size="small" type="info">+ 关注</van-button>
+        <van-button round size="small" type="info">{{article.is_followed?'取消关注':'+ 关注'}}</van-button>
       </div>
       <div class="content">
-        <p>文章内容文章内容文章内容</p>
+        <p v-html="article.content"></p>
       </div>
       <div class="zan">
+        <!--
+          class="active" 代表激活红色的样式
+              通过attitude属性值决定当前按钮是否有红框
+          plain： 表示按钮没有背景色，就是白底，但是有边框
+          icon="like-o" 给按钮设置图标
+        -->
         <van-button
           round
           size="small"
-          class="active"
+          :class="{active:article.attitude===1}"
           plain
           icon="like-o"
           style="margin-right:8px;"
         >点赞</van-button>
-        <van-button round size="small" plain icon="delete">不喜欢</van-button>
+        <van-button
+          round
+          size="small"
+          :class="{active:article.attitude===0}"
+          plain
+          icon="delete"
+        >不喜欢</van-button>
       </div>
     </div>
   </div>
@@ -72,25 +78,25 @@ export default {
 }
 .detail {
   padding: 0 20px 88px;
-  margin-top:92px;
+  margin-top: 92px;
   .title {
     font-size: 36px;
     line-height: 2;
   }
-  .zan{
+  .zan {
     text-align: center;
     padding: 20px 0;
-    .active{
-      border-color:red;
+    .active {
+      border-color: red;
       color: red;
     }
   }
   .author {
     padding: 20px 0;
     display: flex;
-    position:sticky;
+    position: sticky;
     background-color: #fff;
-    top:92px;
+    top: 92px;
     z-index: 2;
     .text {
       flex: 1;
@@ -112,14 +118,14 @@ export default {
     overflow: hidden;
     white-space: pre-wrap;
     word-break: break-all;
-    p{
-      font-size:28px;
+    p {
+      font-size: 28px;
     }
-    /deep/ img{
-      max-width:100%;
+    /deep/ img {
+      max-width: 100%;
       background: #f9f9f9;
     }
-    /deep/ code{
+    /deep/ code {
       white-space: pre-wrap;
     }
   }
