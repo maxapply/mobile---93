a7d88c4ce0edd5407c4cf4af8001a7539d3a1782
sunshuhua <shuhua141@163.com>
2020-03-13 11:00:58 +0800

    文章评论-内容渲染-展示数据

--- src/views/article/components/com-comment.vue
+++ src/views/article/components/com-comment.vue
@@ -2,34 +2,37 @@
   <div class="comment">
     <!--van-list：实现瀑布加载效果-->
     <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <van-cell v-for="item in commentList" :key="item">
+      <van-cell v-for="item in commentList" :key="item.com_id.toString()">
         <!-- 作者头像
           slot="icon" 是cell单元格命名插槽，自定义左侧图标
         -->
         <div slot="icon">
-          <img class="avatar" src="http://toutiao.meiduo.site/Fn6-mrb5zLTZIRG3yH3jG8HrURdU" alt>
+          <img class="avatar" :src="item.aut_photo" alt>
         </div>
         <!-- 作者名称
           slot="title" 是cell单元格命名插槽，左侧标题内容
         -->
         <div slot="title">
-          <span>只是为了好玩儿</span>
+          <span>{{item.aut_name}}</span>
         </div>
         <!-- 点赞
           slot="default" 是cell单元格命名插槽，右侧内容
+          danger:点赞，红色
+          default:没有点赞，没有特殊颜色
         -->
         <div slot="default">
-          <van-button icon="like-o" size="mini" plain>&nbsp;12</van-button>
+          <van-button icon="like-o" size="mini" plain
+          :type="item.is_liking?'danger':'default'">&nbsp;{{item.like_count}}</van-button>
         </div>
         <!-- 评论内容和时间
           slot="label" 是cell单元格命名插槽，下方描述信息
         -->
         <div slot="label">
-          <p>hello world</p>
+          <p>{{item.content}}</p>
           <p>
-            <span>2019-7-17 14:08:20</span>
+            <span>{{item.pubdate | formatTime}}</span>
             ·
-            <span>4&nbsp;回复</span>
+            <span>{{item.reply_count}}&nbsp;回复</span>
           </p>
         </div>
       </van-cell>
