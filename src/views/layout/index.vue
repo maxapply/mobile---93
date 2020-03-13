150719eaba833d255450732136f587d882a1cca3
sunshuhua <shuhua141@163.com>
2020-03-13 15:48:03 +0800

    个人中心-不同状态设置

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -26,7 +26,9 @@
       <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item to="/question"  icon="chat-o">问答</van-tabbar-item>
       <van-tabbar-item to="/video"  icon="video-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/user"  icon="user-o">我的</van-tabbar-item>
+      <van-tabbar-item :to="$store.state.user.token?'/user':'/login'"  icon="user-o">
+        {{$store.state.user.token?'我的':'未登录'}}
+      </van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
