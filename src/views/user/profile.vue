a16402423409e580872f7ccd78d836fef7c99a1a
sunshuhua <shuhua141@163.com>
2020-03-14 11:07:43 +0800

    编辑资料-子级弹层-名称和性别

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -28,8 +28,8 @@
             :src="userprofile.photo"
           ></van-image>
         </van-cell>
-        <van-cell title="名称" is-link :value="userprofile.name"></van-cell>
-        <van-cell title="性别" is-link :value="userprofile.gender===0?'男':'女'"></van-cell>
+        <van-cell title="名称" is-link :value="userprofile.name" @click="showName=true"></van-cell>
+        <van-cell title="性别" is-link :value="userprofile.gender===0?'男':'女'" @click="showSex=true"></van-cell>
         <van-cell title="生日" is-link :value="userprofile.birthday"></van-cell>
       </van-cell-group>
 
@@ -40,6 +40,24 @@
         <van-cell title="本地相册选择图片" is-link></van-cell>
         <van-cell title="拍照" is-link></van-cell>
       </van-popup>
+
+      <!-- 名称弹出层
+        高度不配置，通过内容自动填充
+      -->
+      <van-popup v-model="showName" position="bottom" style="height:20%">
+        <!-- 通过v-model把当前用户真实的名字表现出来
+          .trim ： 自动去除左右空格
+        -->
+        <van-field v-model.trim="userprofile.name" type="text"></van-field>
+      </van-popup>
+
+      <!-- 性别弹出层(上拉菜单)
+        v-model="showSex" 设置弹层是否显示
+        :actions="sexMenus" 设定上拉菜单项目的
+        @select="onSelect" 单击到某一个菜单项目后的回调处理：收起菜单，选中项目
+        cancel-text="取消" 设置有取消按钮提示
+      -->
+      <van-action-sheet v-model="showSex" :actions="sexMenus" @select="onSelect" cancel-text="取消"/>
   </div>
 </template>
 
@@ -50,7 +68,15 @@ export default {
   name: 'user-profile',
   data () {
     return {
+      // 给性别上拉菜单 配置选取项目，语法结构固定，name属性固定
+      sexMenus: [
+        { name: '男' },
+        { name: '女' }
+      ],
+      showSex: false, // 名称菜单层显示开关
+      showName: false, // 名称弹出层显示开关
       showPhoto: false, // 头像弹出层显示开关
+
       // 用户资料的对象成员
       /**
           ├─ id integer 必须  用户id
@@ -67,6 +93,16 @@ export default {
     this.getUserProfile()
   },
   methods: {
+    // 性别 上拉菜单 选中项目 的回调处理
+    // val: 代表被选中项目的对象数据
+    onSelect (val) {
+      console.log(val) // {name:'男'}
+      // 对val进行处理，然后赋值给data表单成员(其接受数字的性别)
+      this.userprofile.gender = val.name === '男' ? 0 : 1
+
+      // 隐藏菜单
+      this.showSex = false
+    },
     // 获得资料
     async getUserProfile () {
       // data直接接收数据
